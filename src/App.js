39f1adc5533756153fdaa3a3f6ac69f4147c3134
Adam Barton <adamb6102@gmail.com>
2020-02-08 16:19:50 -0800

    Added in third loader.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import PulseBar1 from './loaders/pulseBar1.js';
 import PulseBar2 from './loaders/pulseBar2.js';
+import PulseBubble1 from './loaders/pulseBubble1.js';
 
 function App() {
   return (
     <div>
       <PulseBar1 />
       <PulseBar2 />
+      <PulseBubble1 />
     </div>
   );
 }
