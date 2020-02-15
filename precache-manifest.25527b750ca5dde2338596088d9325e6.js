544d9449445223df6948588e92a3604415dd280e
Adam Barton <adamb6102@gmail.com>
2020-02-15 14:23:11 -0800

    Updates

---
+++ precache-manifest.25527b750ca5dde2338596088d9325e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "062d62a16bf939c0534cdd34a81c82e1",
+    "url": "/css-loaders/index.html"
+  },
+  {
+    "revision": "59cabc25f8687d3b726f",
+    "url": "/css-loaders/static/js/2.9c373387.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/css-loaders/static/js/2.9c373387.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3101a29cb178a4371cc",
+    "url": "/css-loaders/static/js/main.b42ef947.chunk.js"
+  },
+  {
+    "revision": "951017b99e3957bedb12",
+    "url": "/css-loaders/static/js/runtime-main.fc0db280.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/css-loaders/precache-manifest.25527b750ca5dde2338596088d9325e6.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/css-loaders/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
